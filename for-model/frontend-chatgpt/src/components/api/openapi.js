f862ad1eca19abcb5dbbc6f2f74634d2a2ce1634
dhtns818 <dhtns818@rosesystems.kr>
2023-11-22 18:10:07 +0900

    commit

--- for-model/frontend-chatgpt/src/components/api/openapi.js
+++ for-model/frontend-chatgpt/src/components/api/openapi.js
@@ -1,7 +1,22 @@
-import yamlData from "js-yaml-loader!../api/openapi.yaml";
+forEach: Model
+fileName: openapi.js
+---
+{{#boundedContexts}}
+    {{#aggregates}}
+import {{nameCamelCase}}Yaml from "js-yaml-loader!../api/{{nameCamelCase}}-openapi.yaml";
+    {{/aggregates}}
+{{/boundedContexts}}
 
 const YAML = require('json2yaml');
 
-let apiSpec = YAML.stringify(yamlData);
+let apiSpec = "";
+
+{{#boundedContexts}}
+    {{#aggregates}}
+let {{nameCamelCase}}Spec = YAML.stringify({{nameCamelCase}}Yaml);
+apiSpec += {{nameCamelCase}}Spec;
+
+    {{/aggregates}}
+{{/boundedContexts}}
 
 export default apiSpec;
